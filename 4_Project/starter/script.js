4e589d351b152302421eef18800f8e8dbb51601d
NIMANTYPOR <samirhasanov@yahoo.com>
2023-06-01 18:29:55 +0400

    commit

--- 4_Project/starter/script.js
+++ 4_Project/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const arr = [2, 3, 4];
+const a = arr [0];
+const b = arr[1];
+
+const [x, y, z] = [2, 3, 4];
+console.log(x, y, z);
+
+
+
+
+
+
+
+
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
