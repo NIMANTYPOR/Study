744c2c2cb52e579d9abfaff8eff038a2c33c28ca
Lenovo_laptop <samirhasanov@yahoo.com>
2023-06-14 18:00:22 +0400

    message

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  } ,
 
   openingHours: {
     thu: {
@@ -27,3 +30,34 @@ const restaurant = {
     },
   },
 };
+// let [main, secondary] = restaurant.categories;
+// console.log (main, secondary); 
+
+
+// [main, secondary] = [secondary, main];
+// console.log (main, secondary);
+// const [starter, mainCourse] = restaurant.order (2, 0)
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6] ];
+// const [i,m , [o, k]] = nested;
+// console.log(i, o , k,m);
+
+const { name, openingHours, categories} = restaurant;
+console.log( name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, openingHours);
+
+const {
+  menu = [],
+  starterMenu: starters = []
+} = restaurant;
+console.log(menu, starters);
+
+
+ 
